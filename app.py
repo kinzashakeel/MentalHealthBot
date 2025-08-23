ce885caef877a3a5c2d7558c4cc5f1af233bb234
Kinza Shakeel <36408462+kinzashakeel@users.noreply.github.com>
2025-08-23 18:54:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,123 +1,4 @@
-from openai import OpenAI
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chains import RetrievalQA
-from langchain_community.embeddings import OpenAIEmbeddings
-from dotenv import load_dotenv
-import pandas as pd
-import PyPDF2
-import os
-from sentence_transformers import SentenceTransformer
-
-load_dotenv() 
-
-OPENAI_API_KEY= os.getenv('OPENAI_API_KEY')
-client = OpenAI(api_key=OPENAI_API_KEY)
-
-
-def extract_text_from_pdf(file_path):
-    
-    from langchain.document_loaders import PyPDFLoader
-   
-    # Load PDF
-    loaders = [
-       
-        PyPDFLoader(file_path)
-    ]
-    docs = []
-    for loader in loaders:
-        docs.extend(loader.load())
-    
-        # Split
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1500,
-        chunk_overlap = 150
-    )
-    splits = text_splitter.split_documents(docs)
-    
-    print(splits)
-    return splits
-
-# Function to read data from a CSV file and create a vector store
-def create_vector_store(file_path):
-    
-    splits=extract_text_from_pdf(file_path)
-    __import__('pysqlite3')
-    
-    
-  
-    persist_directory = 'chroma.sqlite3'
-    
-    embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
-    
-    
-    vector_store = Chroma.from_documents(
-    documents=splits,
-    embedding=embeddings,
-    persist_directory=persist_directory
-)
-    
-    return vector_store
-
-
-def generate_response(prompt, vector_store):
-    try:
-        # Search for relevant documents using the vector store
-        docs = vector_store.similarity_search(query=prompt, k=5)
-
-        # Combine the retrieved documents with the prompt
-        context = "\n".join([doc.page_content for doc in docs])
-
-        combined_prompt = [
-            {
-                "role": "system",
-                "content": """
-                Chat and respond in an informal style like humans chat with each other. Use emojis and a friendly tone. 
-                Make it a conversation bot. Your responses should be formatted like a message in a messaging app. No special characters.
-                Don't add Don't include anything like "Answer:" in start of response message. Just chat humanly. Greet only once at the start of the conversation; 
-                don't say hi/hello/welcome/greetings again and again. Only Talk regarding mental health strictly, no other topic discussion.
-
-                Act like a psychologist/therapist whose sole purpose is to provide support and guidance to individuals struggling with their mental health. 
-                Your primary goal is to assess their mental health condition and offer effective countermeasures to improve their well-being.
-
-                Engage in empathetic and non-judgmental conversations, actively listening to patients' concerns, understanding their emotions, 
-                and providing insightful feedback. You should be equipped with the ability to identify different mental health conditions 
-                such as anxiety, depression, or stress, and recommend appropriate coping strategies, self-care techniques, or professional help if necessary.
-
-                Ask the patient's country first. Then talk with the patient according to the cultural issues of that region, city, or country.
-                Only give short but meaningful responses. Only give one message at a time and wait for the patient's response; 
-                make it like a conversation. Don't ask too many questions; it may irritate the patient. Only ask basic questions to know 
-                the patient's mental health condition and then suggest remedies and measures for betterment of that specific mental health issue.
-                
-                Also, suggest any online psychologist service according to the region, city, or country of the patient. 
-                Make messages short; don't give long responses. Reply messages in Urdu if the user's message is in Urdu; 
-                otherwise, reply messages in English. 
-                {context}
-                Question: {prompt}
-                """
-            }
-        ]
-
-        # Insert the context and user prompt into the message content
-        combined_prompt[0]["content"] = combined_prompt[0]["content"].format(context=context, prompt=prompt)
-
-        # Generate a response using GPT-4
-        response = client.chat.completions.create(
-            model="gpt-4",
-            messages=combined_prompt,
-            max_tokens=1024,
-            temperature=0.7
-        )
-
-        # Extract the content from the response
-        return response.choices[0].message.content
-
-    except Exception as e:
-        print("Error:", e)
-        return "I couldn't process your request. Please try again later."
-
-
+import google.generativeai as genai
 import streamlit as st
 from streamlit_chat import message
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings
@@ -132,32 +13,37 @@ import base64
 import pyttsx3
 import speech_recognition as sr
 from audio_recorder_streamlit import audio_recorder
-from dotenv import load_dotenv
+import os
+import openai
 
-def getResponse(user_input,vector_store):
-    response=""
-    if user_input.lower() == "quit":
-        response= "Takecare Good Bye!"
-    else:
-         response = generate_response(user_input, vector_store)
-    return response
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]  
+
+openai.api_key = os.getenv(OPENAI_API_KEY)
+#Model Initiation
+
+#model= genai.GenerativeModel("gemini-1.5-flash")
+
+
+def getResponse(user_input):
+
+    test_messages = []
+
+    system_message = "First ask user about country and language preference for chatting. we have only two options 1. English and 2. Urdu. Whichever language user selects, only reply in that language.You are a mental health support chatbot acting as a friendly therapist and psychologist and you have to do conversation with patients aasking them about their mental health issues, but do not ask too many questions, just ask 2,3 questions and give your detailed solution regarding that specific mental health problem.Don't change language by yourself until user asks to speak in that language.Continue communication in same language.Don't say anything unnecessary. Do not repeat any question again. If user input is in English reply in English, if user input is in Urdu Language then only reply in Urdu Language Otherwise only use English Language by default. Just reply like a human psychologist.Reply detailed satisfactory answers in both languages. If same qustions is asked again, try to answer with different wordings but the context of the question and answer should be retained.Also, make output responses shorter upto 100 words at maximum. Like humans do chatting. If you do not understand user input, just say nicely to user to repeat the question again. Do not answer questions outside the domain of mental health. Avoid question having keywords like suicide, just refer the user to online links to book appointment with a psychologist."
+    test_messages.append({"role": "system", "content": system_message})
+       
+
+    test_messages.append({"role": "system", "content": user_input})
+        #OpenAI Chat Completions
+    response = openai.ChatCompletion.create(
+                #model='ft:gpt-4o-mini-2024-07-18:sukkur-iba:mentalhealth:AGTajjiH', #can test it against gpt-3.5-turbo to see difference
+                model= "gpt-5",
+                messages=test_messages,
+                temperature=1,
+                max_completion_tokens=500
+        )
+    return response["choices"][0]["message"]["content"]
 
 import tempfile
-
-
-def recognize_speech(temp_urdu_recording_path):
-
-                    recognizer = sr.Recognizer()
-                    with sr.AudioFile(temp_urdu_recording_path) as source:
-                        urdu_recoded_voice = recognizer.record(source)
-                        try:
-                            text = recognizer.recognize_google(urdu_recoded_voice, language="en")
-                            return text
-                        except sr.UnknownValueError:
-                            return "آپ کی آواز واضح نہیں ہے"
-                        except sr.RequestError:
-                            return "Sorry, my speech service is down"
-                        
 def speak_text(text):
     """Function to convert text to speech and play it."""
     tts = gTTS(text=text, lang='en')
@@ -175,38 +61,32 @@ def speak_text(text):
 
 
 
-
 def handle_text_input(user_input):
-
-    st.session_state.text_input = ""
-
-
-    # Generate chatbot response
-    file_path = "mh.pdf"  
-    vector_store = create_vector_store(file_path)
-    response = getResponse(user_input,vector_store)
     
+    st.session_state.text_input = ""
+   
+              
+            # Generate chatbot response
+    response = getResponse(user_input)
+            
     print(response)
             # Append user message to chat history
     st.session_state.messages.append({"role": "user", "content": user_input})
     st.session_state.messages.append({"role": "assistant", "content": response})
-
+            
     st.text_input = ""
-
+    
+        
+            
 
 def handle_voice_input(speech_text):
-    
-        # Generate chatbot response
-        file_path = "mh.pdf"  
-        vector_store = create_vector_store(file_path)
-        response = getResponse(speech_text,vector_store)
-        
-        #if st.button("Speak"):
-        #   speech_text = recognize_speech()
+
+    #if st.button("Speak"):
+    #   speech_text = recognize_speech()
         print(speech_text)
         
         st.session_state.messages.append({"role": "user", "content": speech_text})
-        
+        response = getResponse(speech_text)
         st.session_state.messages.append({"role": "assistant", "content": response})
         # Speak the response
         speak_text(response)
@@ -214,7 +94,7 @@ def handle_voice_input(speech_text):
 
 def main():
     """Main function to run the Streamlit app."""
-    st.title("Text and Voice Chatbot using Gemini-1.5-flash & Streamlit")
+    st.title("Mental Health Chatbot")
 
     # Initialize session state for chat history and text input
     if 'messages' not in st.session_state:
@@ -268,6 +148,4 @@ def main():
 if __name__ == "__main__":
     main()
     
-
-
 
